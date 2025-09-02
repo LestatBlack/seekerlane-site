568a8423875d365eb19cfa006b350abc047b576a
LestatBlack <49568134+LestatBlack@users.noreply.github.com>
2025-09-01 20:37:47 -0500

    Update page.tsx

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,1 +1,9 @@
+export default function JobsPage() {
+  return (
+    <section>
+      <h1 className="text-3xl font-bold text-black">Jobs</h1>
+      <p className="text-black">If you see this, the /jobs route works!</p>
+    </section>
+  );
+}
 
